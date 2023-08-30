9d9687c93a1ef0702baf89afb34befed5b8843e3
Kasam Mapara <kasam.mapara@somaiya.edu>
2023-08-30 17:21:41 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,197 +12,118 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-
-        body:
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      // appBar: AppBar(
+      //   title: Center(child: Text("Login")),
+      //   backgroundColor: Colors.orangeAccent ,
+      // ),
+    //
+      body:
+      Padding(
+        padding: const EdgeInsets.all(0),
+        child: Stack(
           children: [
             Container(
-              child: Stack(
-                children: [
-                  Positioned(
-                      child:
-                  Image.asset("lib/assets/img_back.jpg")
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 20, 20, 40),
-              child: Container(
-                child: Text(
-                  "Registration",
-                  style: TextStyle(
-                    color: Colors.blueAccent,
-                    fontSize: 30
-                  ),
-                ),
-              ),
-            ),
-            Center(
-              child: SizedBox(
-                width: 275,
-                height: 400,
+              width: screenWidth,
+              // height: screenHeight * 0.45,
+              color: Colors.orange,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10, 20, 0, 0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+
                   children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 130, 20, 20),
+                      child: Text("Login",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold
+                      ),
+                      ),
 
-                    Expanded(child:
-                    TextFormField(
 
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                        contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                        hintStyle:
-                        TextStyle(
-                          fontSize:20,
-                          color: Colors.white
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 40),
+                      child: Text("Welcome back ! Enter your Details",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
 
                         ),
-                        filled: true,
-                        fillColor: Colors.blue,
-                        hoverColor: Colors.cyan,
-                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                        hintText: 'Username',
                       ),
                     )
-                    ),
-                    Container(
-                      child:
-                      Expanded(
-                        child:TextFormField(
-
-                          textAlign: TextAlign.center,
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                            hintStyle:
-                            TextStyle(
-                                fontSize:20,
-                                color: Colors.white
-
-                            ),
-                            filled: true,
-                            fillColor: Colors.blue,
-                            hoverColor: Colors.cyan,
-                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                            hintText: 'Full Name',
-                          ),
-                        ) ,
-                      ),
-                    ),
-                    Container(
-                      child:
-                      Expanded(
-                        child:TextFormField(
-
-                          textAlign: TextAlign.center,
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                            hintStyle:
-                            TextStyle(
-                                fontSize:20,
-                                color: Colors.white
-
-                            ),
-                            filled: true,
-                            fillColor: Colors.blue,
-                            hoverColor: Colors.cyan,
-                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                            hintText: 'Email',
-                          ),
-                        ) ,
-                      ),
-                    ),Container(
-                      child:
-                      Expanded(
-                        child:TextFormField(
-
-                          textAlign: TextAlign.center,
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                            hintStyle:
-                            TextStyle(
-                                fontSize:20,
-                                color: Colors.white
-
-                            ),
-                            filled: true,
-                            fillColor: Colors.blue,
-                            hoverColor: Colors.cyan,
-                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                            hintText: 'Phone Number',
-                          ),
-                        ) ,
-                      ),
-                    ),
-                        Container(
-
-                     child: Row(
-                       // mainAxisAlignment: MainAxisAlignment.center,
-                       crossAxisAlignment: CrossAxisAlignment.center,
-                       children: [
-                         Padding(
-                           padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                           child: Text("Gender",
-                             textAlign: TextAlign.left,
-                             style: TextStyle(
-                               fontSize: 20,
-
-                             ),
-                           ),
-                         ),
-                         OutlinedButton(
-                           style: OutlinedButton.styleFrom(
-                             backgroundColor: Colors.blue
-                           ),
-                           onPressed: (){}, child:
-                         Text("Male", style: TextStyle(color: Colors.white),),
-
-                         )
-
-
-                       ],
-
-                     ),
-                        )
-                    ,
-                    Container(
-                      child:
-                      Expanded(
-                        child:TextFormField(
-
-                          textAlign: TextAlign.center,
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                            hintStyle:
-                            TextStyle(
-                                fontSize:20,
-                                color: Colors.white
-
-                            ),
-                            filled: true,
-                            fillColor: Colors.blue,
-                            hoverColor: Colors.cyan,
-                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                            hintText: 'Password',
-                          ),
-                        ) ,
-                      ),
-                    )
-
                   ],
                 ),
+
+              ),
+
+
+            ),
+            Positioned(
+              top: screenHeight * 0.40,
+              // bottom: screenHeight,
+               height: screenHeight,
+              child: Container(
+                width: screenWidth,
+                height: screenHeight * 0.55,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50)
+                ),
+                child:
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 100, 30, 10),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.email),
+
+                          hintText: 'Email',
+                          hintStyle: TextStyle(
+                            fontSize: 20,
+                            // fontWeight: FontWeight.bold
+                          )
+                        )
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      TextFormField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              icon: Icon(Icons.password),
+
+                              hintText: 'Password',
+                              hintStyle: TextStyle(
+                                  fontSize: 20,
+                                  // fontWeight: FontWeight.bold
+                              )
+                          )
+                      ),
+                      ElevatedButton(onPressed: (){}, child: Text("Login"
+                          ,
+                      ))
+
+                    ],
+
+                  ),
+
+                ),
+
+
               ),
             )
           ],
-        )
+        ),
+      ),
 
-
-
-
-
-
-
-    );
+     );
   }
 }
